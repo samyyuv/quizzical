940040bff86651db0755d21616710eb0f1e9f2d8
samyyuv <samyy.uv@gmail.com>
2022-08-03 15:55:36 +0200

    styling presentation page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import './App.scss';
+import Start from './components/Start';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
+      <div className="circle circle-big up"></div>
+      <div className="circle circle-small small-up"></div>
+      <Start />
+      <div className="circle circle-small small-down"></div>
+      <div className="circle circle-big down"></div>
+
     </div>
   );
 }
---
+++ src/components/Start.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Start() {
+  return (
+    <div className='start-container'>
+      <h1 className="title font-karla">Quizzical</h1>
+      <p className="description font-inter">Some description if needed</p>
+      <button className="hard-button font-inter">Start quiz</button>
+    </div>
+  )
+}
